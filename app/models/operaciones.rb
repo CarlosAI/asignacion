4e9f28839f0c03b3c3ee0df2703a13a540a8eb64
CarlosAI <carloswesker@outlook.es>
2019-11-02 15:35:14 -0600

    error

--- app/models/operaciones.rb
+++ app/models/operaciones.rb
@@ -13,27 +13,30 @@ class Operaciones
 		shipments.each do |shp|
 			puts "Vamos con el tracking_number #{shp["tracking_number"]}"
 			results = fedex.track(:tracking_number => shp["tracking_number"])
-
-			tracking_info = results.first
-			a = tracking_info.to_json
-			res = JSON.parse a
-			shp["fedex_parcel"] = {}
-			if res["details"]["notification"]["severity"] == "SUCCESS"
-				detalles = res["details"]
-				detalles["package_weight"]
-				if detalles["package_weight"]["units"] == "LB"
-					shp["fedex_parcel"]["weight"] = (2.54)*(detalles["package_weight"]["value"].to_f)
+			if results.present?
+				tracking_info = results.first
+				a = tracking_info.to_json
+				res = JSON.parse a
+				shp["fedex_parcel"] = {}
+				if res["details"]["notification"]["severity"] == "SUCCESS"
+					detalles = res["details"]
+					detalles["package_weight"]
+					if detalles["package_weight"]["units"] == "LB"
+						shp["fedex_parcel"]["weight"] = (2.54)*(detalles["package_weight"]["value"].to_f)
+					else
+						shp["fedex_parcel"]["weight"] = detalles["package_weight"]["value"].to_f
+					end
+					if detalles["package_dimensions"]["units"] == "IN"
+						shp["fedex_parcel"]["length"] = (0.453592)*(detalles["package_dimensions"]["length"].to_f)
+						shp["fedex_parcel"]["width"] = (0.453592)*(detalles["package_dimensions"]["width"].to_f)
+						shp["fedex_parcel"]["height"] = (0.453592)*(detalles["package_dimensions"]["height"].to_f)
+					else
+						shp["fedex_parcel"]["length"] = detalles["package_dimensions"]["length"].to_f
+						shp["fedex_parcel"]["width"] = detalles["package_dimensions"]["width"].to_f
+						shp["fedex_parcel"]["height"] = detalles["package_dimensions"]["height"].to_f
+					end
 				else
-					shp["fedex_parcel"]["weight"] = detalles["package_weight"]["value"].to_f
-				end
-				if detalles["package_dimensions"]["units"] == "IN"
-					shp["fedex_parcel"]["length"] = (0.453592)*(detalles["package_dimensions"]["length"].to_f)
-					shp["fedex_parcel"]["width"] = (0.453592)*(detalles["package_dimensions"]["width"].to_f)
-					shp["fedex_parcel"]["height"] = (0.453592)*(detalles["package_dimensions"]["height"].to_f)
-				else
-					shp["fedex_parcel"]["length"] = detalles["package_dimensions"]["length"].to_f
-					shp["fedex_parcel"]["width"] = detalles["package_dimensions"]["width"].to_f
-					shp["fedex_parcel"]["height"] = detalles["package_dimensions"]["height"].to_f
+					trackings_error << shp["tracking_number"]
 				end
 			else
 				trackings_error << shp["tracking_number"]
