efbe954067e1efbca229ec29d96c62ec4bc5112a
CarlosAI <carloswesker@outlook.es>
2019-11-02 12:51:46 -0600

    credentials

--- app/models/operaciones.rb
+++ app/models/operaciones.rb
@@ -1,8 +1,18 @@
 class Operaciones
 
-	def self.requestFedex(params)
+	def self.requestFedex(shipments)
 		puts "Entramos a fedex y los params son"
-		puts params
+		puts shipments
+
+		require 'fedex'
+		fedex = Fedex::Shipment.new(:key => ENV.fetch('FEDEX_KEY'),
+		                            :password => ENV.fetch('FEDEX_PASSWORD'),
+		                            :account_number => ENV.fetch('FEDEX_ACCOUNT'),
+		                            :meter => ENV.fetch('FEDEX_METER'),
+		                            :mode => ENV.fetch('FEDEX_MODE')
+		                            )
+		results = fedex.track(:tracking_number => "1234567890123")
+		
 		return 200
 	end
 end
